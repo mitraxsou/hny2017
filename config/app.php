487c7a93c5315bf2996369da2ccd172e917b5760
soumitra mandal <mitraxsou@gmail.com>
2018-01-07 20:52:06 +0530

    final version of hny 2017 app and ready for release

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Stevebauman\Location\LocationServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +227,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+
+        'Location' => Stevebauman\Location\Facades\Location::class,
 
     ],
 
